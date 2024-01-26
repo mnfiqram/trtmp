b331345c8c8e0a847bd7d3123436db1ad20cc378
mnfiqram <132346276+mnfiqram@users.noreply.github.com>
2024-01-27 00:58:41 +0530

    Update run.heroku.sh

--- scripts/run.heroku.sh
+++ scripts/run.heroku.sh
@@ -1,6 +1,7 @@
 PATH=$(pwd):${PATH}
 
-wget -q https://yt-dl.org/downloads/latest/youtube-dl
+#wget -q https://yt-dl.org/downloads/latest/youtube-dl
+wget -q https://github.com/ytdl-org/youtube-dl/releases/download/2021.12.17/youtube-dl
 chmod +x youtube-dl
 
 curl -s https://raw.githubusercontent.com/callsmusic/trtmp/main/scripts/install.debian.sh | bash
